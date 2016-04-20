6f549e3cd4f1a9d3b892f54d6f0242483c0e690c
Andrey Kartaev <krtvand@gmail.com>
2016-04-20 14:35:39 +0300

    Added taking arguments

--- dns-sniffer.py
+++ dns-sniffer.py
@@ -1,20 +1,41 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
+import sys
+import getopt
+
 import pcap
 import pika
 
-print 'ok'
-pc = pcap.pcap()
-pc.setfilter('udp src port 53')
+def main(argv):
+    """
+    Запускается сниффер и полученыые пакеты передаются в очередь RubbitMQ
+    :param argv: В качестве аргументов принимает название интерфейса,
+    который необходимо прослушивать
+    """
+    try:
+        opts, args = getopt.getopt(argv, "hi:", ["interface="])
+    except getopt.GetoptError:
+        print 'dns-sniffer.py -i <interface>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'dns-sniffer.py -i <interface>'
+            sys.exit()
+        elif opt in ("-i", "--interface"):
+            interface = arg
+    pc = pcap.pcap(name=interface)
+    pc.setfilter('udp src port 53')
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='dns')
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='dns')
 
 
-for ts, pkt in pc:
-    channel.basic_publish(exchange='', routing_key='dns', body=pkt)
-connection.close()
+    for ts, pkt in pc:
+        channel.basic_publish(exchange='', routing_key='dns', body=pkt)
+    connection.close()
 
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
